8800a833223b2a5f40487abe82150fc54678d0fc
yeahChibyke <chibykepride@gmail.com>
2025-04-03 13:55:15 +0100

    done with ground work

--- src/LevelOne.sol
+++ src/LevelOne.sol
@@ -3,9 +3,14 @@ pragma solidity 0.8.26;
 
 // import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title Hawk High First Flight
+ * @author Chukwubuike Victory Chime @yeahChibyke
+ * @notice Contract for the Hawk High School
+ */
 contract LevelOne {
     error HH__NotPrincipal();
-    error HH__NotPrincipalOrTeacher();
+    error HH__NotTeacher();
     error HH__ZeroAddress();
     error HH__TeacherExists();
     error HH__StudentExists();
@@ -13,11 +18,21 @@ contract LevelOne {
     address principal;
     mapping(address => bool) private isTeacher;
     mapping(address => bool) private isStudent;
+    mapping(address => uint256) private studentScore;
+    mapping(address => uint256) private reviewCount;
+    mapping(address => uint256) private lastReviewTime;
+    uint256 public schoolFees;
+    uint256 public immutable reviewTime = 1 weeks;
+    bool inSession;
+    uint256 public sessionEnd;
+    uint256 public bursary;
 
     event TeacherAdded(address indexed);
     event TeacherRemoved(address indexed);
     event Enrolled(address indexed);
     event Expelled(address indexed);
+    event SchoolInSession(uint256 indexed startTime, uint256 indexed endTime, uint256 indexed schoolFees);
+    event ReviewGiven(address indexed student, bool indexed review, uint256 indexed studentScore);
 
     constructor(address _principal) {
         principal = _principal;
@@ -30,14 +45,19 @@ contract LevelOne {
         _;
     }
 
-    modifier onlyStaff() {
-        if (msg.sender == principal || !isTeacher[msg.sender]) {
-            revert HH__NotPrincipalOrTeacher();
+    modifier onlyTeacher() {
+        if (!isTeacher[msg.sender]) {
+            revert HH__NotTeacher();
         }
         _;
     }
 
-    function addTeacher(address _teacher) public onlyPrincipal {
+    modifier notYetInSession() {
+        require(!inSession, "Hawk High is already in session!!!");
+        _;
+    }
+
+    function addTeacher(address _teacher) public onlyPrincipal notYetInSession {
         if (_teacher == address(0)) {
             revert HH__ZeroAddress();
         }
@@ -65,14 +85,17 @@ contract LevelOne {
         emit TeacherRemoved(_teacher);
     }
 
-    function enroll() external {
+    function enroll() external payable notYetInSession {
         require(!isTeacher[msg.sender] && msg.sender != principal);
+        require(msg.value == schoolFees, "Hawk High fees not paid!!!");
 
         if (isStudent[msg.sender]) {
             revert HH__StudentExists();
         }
 
         isStudent[msg.sender] = true;
+        studentScore[msg.sender] = 100;
+        bursary += msg.value;
 
         emit Enrolled(msg.sender);
     }
@@ -86,4 +109,34 @@ contract LevelOne {
 
         isStudent[_student] = false;
     }
+
+    function startSession(uint256 _amount) public onlyPrincipal notYetInSession {
+        if (_amount == 0) {
+            revert HH__ZeroAddress();
+        }
+        sessionEnd = block.timestamp + 4 weeks;
+        inSession = true;
+
+        schoolFees = _amount;
+
+        emit SchoolInSession(block.timestamp, sessionEnd, schoolFees);
+    }
+
+    function giveReview(address _student, bool review) public onlyTeacher {
+        require(reviewCount[_student] < 5, "Student review count exceeded!!!");
+        require(isStudent[_student], "Student does not exist!!!");
+        require(block.timestamp >= lastReviewTime[_student] + reviewTime, "Reviews can only be given once per week");
+
+        // where `false` is a bad review and true is a good review
+        if (!review) {
+            studentScore[_student] -= 10;
+        }
+
+        // Update last review time
+        lastReviewTime[_student] = block.timestamp;
+
+        emit ReviewGiven(_student, review, studentScore[_student]);
+    }
+
+    receive() external payable {}
 }
