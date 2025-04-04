1ebd7245221e5a8d5f7329269640ac874e483ada
yeahChibyke <chibykepride@gmail.com>
2025-04-04 14:36:07 +0100

    done with LevelOne script

--- script/DeployLevelOne.s.sol
+++ script/DeployLevelOne.s.sol
@@ -2,5 +2,18 @@
 pragma solidity 0.8.26;
 
 import {Script} from "forge-std/Script.sol";
+import {LevelOne} from "../src/LevelOne.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-contract DeployLevelOne is Script {}
+contract DeployLevelOne is Script {
+    function run() external returns (address proxy) {
+        proxy = deployLevelOne();
+        return proxy;
+    }
+
+    function deployLevelOne() public returns (address) {
+        LevelOne levelOne = new LevelOne();
+        ERC1967Proxy proxy = new ERC1967Proxy(address(levelOne), "");
+        return address(proxy);
+    }
+}
